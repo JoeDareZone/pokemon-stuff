f04af2b16bb572d7af6df256d598dcc1fa383681
Joe Ulyatt <joe@dare.zone>
2024-07-07 17:14:03 +0200

    style: :art: Improve schema file formatting

--- models/pokemon.schema.ts
+++ models/pokemon.schema.ts
@@ -10,7 +10,9 @@ export const PokemonSchema = z.object({
   id: z.number(),
   name: z.string(),
   types: z.array(PokemonTypeSchema),
-  sprites: z.object({ front_default: z.string() }),
+  sprites: z.object({
+    front_default: z.string(),
+  }),
 });
 
 export const PokemonListSchema = z.array(
